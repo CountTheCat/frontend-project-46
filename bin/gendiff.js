23d98e6728cdd8b8ad71c66f6c1eec77360f50fe
KOT <graffdekot027@gmail.com>
2026-03-26 20:16:02 +0300

    modified gendiff

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -2,6 +2,7 @@
 
 import { program } from 'commander'
 import { createRequire } from 'module'
+import genDiff from '../index.js'
 
 const require = createRequire(import.meta.url)
 const packageJson = require('../package.json')
@@ -9,4 +10,9 @@ const packageJson = require('../package.json')
 program
   .version(packageJson.version)
   .description('Compares two configuration files and shows a difference.')
+  .arguments('<filepath1> <filepath2>')
+  .action((filepath1, filepath2) => {
+    const result = genDiff(filepath1, filepath2)
+    console.log(result)
+  })
   .parse()
