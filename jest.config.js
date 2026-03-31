a682dc9952d2be74fafd7bd9d2047594efd36ac0
KOT <graffdekot027@gmail.com>
2026-03-31 18:36:12 +0300

    Add jest.config.js for coverage configuration

---
+++ jest.config.js
@@ -0,0 +1,20 @@
+/** @type {import('jest').Config} */
+export default {
+  testEnvironment: 'node',
+  // Тестовые файлы
+  testMatch: ['**/__tests__/**/*.test.js'],
+  // Генерация покрытия кода
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageReporters: ['json', 'lcov', 'text', 'clover'],
+  // Какие файлы включать в покрытие
+  collectCoverageFrom: [
+    'src/**/*.js',
+    'index.js',
+    '!**/__tests__/**',
+    '!**/__fixtures__/**',
+    '!**/node_modules/**',
+    '!**/bin/**',
+  ],
+  transform: {},
+}
